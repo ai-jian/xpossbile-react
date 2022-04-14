8db9bf1342a75b34a55e4c6f12d1afc68b2f5447
Ai Jian <53556553+ai-jian@users.noreply.github.com>
2022-04-13 23:42:39 -0500

    fix footer responsiveness

--- src/container/Footer/Footer.js
+++ src/container/Footer/Footer.js
@@ -11,10 +11,13 @@ const Footer = () => {
   return (
     <footer id="app__footer">
       <div className="footer-content">
-
-        <img className="footer-logo" src={images.logo} alt="logo" />
-        <p className="footer-email"><a href="mailto:xpossiblestudio@gmail.com">xpossiblestudio@gmail.com</a></p>
-        <p>Find out more</p>
+        {/* <img className="footer-logo" src={images.logo} alt="logo" /> */}
+        <h1 className="footer-title">Let's get started!</h1>
+        <div className="footer-email">
+          <AiOutlineMail className="mailicon" />
+          <a href="mailto:xpossiblestudio@gmail.com">xpossiblestudio@gmail.com</a>
+        </div>
+        <p className="footer-find-text">Find out more</p>
         <div className="footer-social-icons">
           <AiOutlineInstagram className="social-icon" />
           <AiOutlineFacebook className="social-icon" />
@@ -22,13 +25,14 @@ const Footer = () => {
         </div>
 
         {/* footer navigation */}
-        {/* <ul className="footer-nav">
-        {['home', 'about', 'contact', 'showcase', 'service'].map((item) => (
-          <li key={item}>
-            <a href={`#${item}`}>{item}</a>
-          </li>
-        ))}
-      </ul> */}
+        <ul className="footer-nav">
+          <li><h3 className="nav-title"> Menu</h3></li>
+          {['home', 'about', 'contact', 'showcase', 'service'].map((item) => (
+            <li key={item}>
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="footer-copyright">
         <hr />
